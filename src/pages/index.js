6d381ed888ef094425b029ac4a4bca7e541d8ab7
clbhll <67708984+clbhll@users.noreply.github.com>
2021-02-28 08:18:20 -0800

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout/layout"
+import SEO from "../components/layout/seo"
+
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </Layout>
+  )
+}
+
+export default IndexPage
